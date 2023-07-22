36b61b55ad25125af096629c32c048039608ac43
diceboi <122260041+diceboi@users.noreply.github.com>
2023-07-23 01:54:16 +0200

    trustindex deleted

--- app/components/trustindex.tsx
+++
@@ -1,8 +0,0 @@
-export default function Trustindex() {
-    return(
-        <>
-            <div className="" src='https://cdn.trustindex.io/loader.js?8e4f65617d71136a3d563a95ef0'></div>
-        </>
-    )
-}
-
