b2fe03dd0fef76aedc109741e659e4cf03d3cf19
diceboi <122260041+diceboi@users.noreply.github.com>
2023-10-17 21:47:29 +0200

    hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ApolloWrapper } from './lib/apollo-wrapper'
 import Footer from './components/footer'
 import MainCTA from './components/MainCTA'
 import Cookies from './components/Cookies'
+import Script from 'next/script'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
@@ -21,6 +22,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script 
+        id='Hotjar' 
+        strategy='afterInteractive' 
+        dangerouslySetInnerHTML={{
+          __html:
+          `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3698579,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}>
+
+        </Script>
       </head>
       <body className={raleway.className} id='__next'>
           <ApolloWrapper>
