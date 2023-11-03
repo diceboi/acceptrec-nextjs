06fe78e3c40dbda59ff51bb95fcfeaf7166c60ae
diceboi <122260041+diceboi@users.noreply.github.com>
2023-11-03 15:12:51 +0100

    people delete

--- app/components/TeamInside.tsx
+++ app/components/TeamInside.tsx
@@ -16,7 +16,7 @@ export default function TeamInside() {
                     </h2>                    
                 </div>                
             </div>
-            <div className='grid lg:grid-cols-3 lg:grid-rows-3 grid-cols-2 w-11/12 lg:w-8/12 m-auto rounded-3xl'>
+            <div className='grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-2 w-11/12 lg:w-8/12 m-auto rounded-3xl'>
 
             <Link href="/about-us/team/mark-pearce" className="recruitment-tile lg:h-[450px] h-[250px] group flex flex-col cursor-pointer ease-out duration-200">
                     <div className="relative flex flex-col justify-end bg-[url('/Mark-front.webp')] bg-cover bg-center bg-opacity-40 h-full p-4 lg:rounded-tl-3xl ease-out duration-200">
@@ -60,20 +60,6 @@ export default function TeamInside() {
                     </div>                    
                 </Link>
 
-                <Link href="/about-us/team/daria-bhavsar" className="recruitment-tile lg:h-[450px] h-[250px] group flex flex-col cursor-pointer ease-out duration-200">
-                    <div className="relative flex flex-col justify-end bg-[url('/Daria_Bhavsar.jpg')] bg-cover bg-center bg-opacity-40 h-full p-4 ease-out duration-200">
-                        <div className="flex flex-nowrap justify-between w-full z-10 ">
-                            <div className="flex flex-col">
-                                <h3 className="text-2xl font-black text-white">Daria Bhavsar</h3>
-                                <p className="font-medium text-white">General Manager</p>
-                            </div>                            
-                            <FiArrowRight className='absolute right-8 bottom-4 recruitment-icon text-white w-8 h-8 opacity-0 group-hover:opacity-100 group-hover:right-4 ease-out duration-200'/>
-                        </div>
-                        <div className='absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-[#000000c0] to-[#0000] opacity-30 group-hover:opacity-100 ease-out duration-200'>
-                        </div>
-                    </div>                    
-                </Link>
-
                 <Link href="/about-us/team/agnieszka-jasinska" className="recruitment-tile lg:h-[450px] h-[250px] group flex flex-col cursor-pointer ease-out duration-200">
                     <div className="relative flex flex-col justify-end bg-[url('/Aggie-Jasinska-f.webp')] bg-cover bg-center bg-opacity-40 h-full p-4 ease-out duration-200">
                         <div className="flex flex-nowrap justify-between w-full z-10 ">
@@ -84,34 +70,6 @@ export default function TeamInside() {
                             <FiArrowRight className='absolute right-8 bottom-4 recruitment-icon text-white w-8 h-8 opacity-0 group-hover:opacity-100 group-hover:right-4 ease-out duration-200'/>
                         </div>
                         <div className='absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-[#000000c0] to-[#0000] opacity-30 group-hover:opacity-100 ease-out duration-200'>
-                        </div>
-                    </div>                    
-                </Link>
-
-                <Link href="/about-us/team/aliona-cozacenco" className="recruitment-tile lg:h-[450px] h-[250px] group flex flex-col cursor-pointer ease-out duration-200">
-                    <div className="relative flex flex-col justify-end bg-[url('/Aliona_Cozacenco_f.webp')] bg-cover bg-center bg-opacity-40 h-full p-4 ease-out duration-200">
-                        <div className="flex flex-nowrap justify-between w-full z-10 ">
-                            <div className="flex flex-col">
-                                <h3 className="text-2xl font-black text-white">Aliona Cozacenco</h3>
-                                <p className="font-medium text-white">Branch Manager</p>
-                            </div>                            
-                            <FiArrowRight className='absolute right-8 bottom-4 recruitment-icon text-white w-8 h-8 opacity-0 group-hover:opacity-100 group-hover:right-4 ease-out duration-200'/>
-                        </div>
-                        <div className='absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-[#000000c0] to-[#0000] opacity-30 group-hover:opacity-100 ease-out duration-200'>
-                        </div>
-                    </div>                    
-                </Link>
-
-                <Link href="/about-us/team/ada-mazurek" className="recruitment-tile lg:h-[450px] h-[250px] group flex flex-col cursor-pointer ease-out duration-200">
-                    <div className="relative flex flex-col justify-end bg-[url('/Ada_Website_Picture.png')] bg-cover bg-center bg-opacity-40 h-full lg:rounded-bl-3xl p-4 ease-out duration-200">
-                        <div className="flex flex-nowrap justify-between w-full z-10 ">
-                            <div className="flex flex-col">
-                                <h3 className="text-2xl font-black text-white">Ada Mazurek</h3>
-                                <p className="font-medium text-white">Industrial Divisional Manager</p>
-                            </div>                            
-                            <FiArrowRight className='absolute right-8 bottom-4 recruitment-icon text-white w-8 h-8 opacity-0 group-hover:opacity-100 group-hover:right-4 ease-out duration-200'/>
-                        </div>
-                        <div className='absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-[#000000c0] to-[#0000] lg:rounded-bl-3xl opacity-30 group-hover:opacity-100 ease-out duration-200'>
                         </div>
                     </div>                    
                 </Link>
