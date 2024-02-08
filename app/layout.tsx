e4f9d0b48b48616f3fd4f98e8873af36dd7bf450
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-08 15:05:22 +0100

    facebook on

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({
             {children}
             <MainCTA/>
             <Footer/>
+            <FacebookMsg />
           </ApolloWrapper>
       </body>
     </html>
