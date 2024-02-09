5a65e2e9c794118b279662ea824df9588381843a
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-09 21:17:11 +0100

    new chatbot route

---
+++ app/api/botpress-new/route.ts
@@ -0,0 +1,40 @@
+import { Resend } from "resend";
+
+import BotpressNewNotification from "@/emails/BotpressNewNotification";
+import { NextResponse } from "next/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(request: Request) {
+  const { name, tel, email, summary } = await request.json();
+
+  try {
+
+  const companyMail = await resend.sendEmail({
+    from: 'Acceptrec.co.uk <hello@acceptrec.co.uk>',
+    to: 'szasz.szabolcs1995@gmail.com',
+    subject: `[Chat] ${name} is contacted via ChatBot`,
+    bcc: 'peter@acceptrec.co.uk',
+    react: BotpressNewNotification({
+        name,
+        tel,
+        email,
+        summary,
+    })
+  });
+  return NextResponse.json({
+    status: 'Ok'
+  }, {
+    status: 200
+  })
+  } catch(e: unknown) {
+    if (e instanceof Error) {
+      console.log(`Failed to send email: ${e.message}`);
+    }
+    return NextResponse.json({
+      error: 'Internal server error.'
+    }, {
+      status: 500
+    })
+  }
+}
---
+++ emails/BotpressNewNotification.tsx
@@ -0,0 +1,115 @@
+import {
+    Body,
+    Container,
+    Head,
+    Heading,
+    Html,
+    Img,
+    Link,
+    Preview,
+    Text,
+  } from '@react-email/components';
+  import * as React from 'react';
+  
+  interface WelcomeEmailProps {
+    name: string;
+    tel: string;
+    email: string;
+    summary: string;
+  }
+  
+  export const ContactUsCompany = ({ name, tel, email, summary }: WelcomeEmailProps) => (
+    <Html>
+      <Head />
+      <Preview>A {name} is contacted via ChatBot.</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Heading style={h1}>A {name} is contacted via ChatBot.</Heading>
+          <Text style={{ ...text, marginBottom: '24px'}}>
+            Please respond to {name} as soon as you can.
+            
+            Name:{name}
+            Phone number: {tel}
+            Email adress: {email}
+            Summary of the conversation: {summary}
+            
+          </Text>
+          <Img
+            src="http://admin.acceptrec.co.uk/wp-content/uploads/2024/01/Accept-Stacked-Logo-with-Strapline-RGB300.png"
+            width="auto"
+            height="50"
+            alt="Acceptrec's logo"
+          />
+          <Text style={footer}>
+            <Link
+              href="https://acceptrec.co.uk"
+              target="_blank"
+              style={{ ...link, color: '#898989' }}
+            >
+              Acceptrec.co.uk
+            </Link>
+            , the highest rated recruitment agency in the UK.
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+  
+  export default ContactUsCompany;
+  
+  const main = {
+    backgroundColor: '#ffffff',
+  };
+  
+  const container = {
+    paddingLeft: '12px',
+    paddingRight: '12px',
+    margin: '0 auto',
+  };
+  
+  const h1 = {
+    color: '#312252',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    fontSize: '36px',
+    fontWeight: 'bold',
+    margin: '40px 0',
+    padding: '0',
+  };
+  
+  const link = {
+    color: '#2754C5',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    fontSize: '14px',
+    textDecoration: 'underline',
+  };
+  
+  const text = {
+    color: '#333',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    fontSize: '14px',
+    margin: '24px 0',
+  };
+  
+  const footer = {
+    color: '#898989',
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    fontSize: '12px',
+    lineHeight: '22px',
+    marginTop: '12px',
+    marginBottom: '24px',
+  };
+  
+  const code = {
+    display: 'inline-block',
+    padding: '16px 4.5%',
+    width: '90.5%',
+    backgroundColor: '#f4f4f4',
+    borderRadius: '5px',
+    border: '1px solid #eee',
+    color: '#333',
+  };
+  
