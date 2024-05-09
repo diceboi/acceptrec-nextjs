2b9c446ac41a56934e900935cb624edcccd39b4b
diceboi <122260041+diceboi@users.noreply.github.com>
2024-05-09 22:01:36 +0200

    maja route

---
+++ app/api/maja/route.ts
@@ -0,0 +1,56 @@
+import { Resend } from "resend";
+
+import ContactUsUser from "@/emails/ContactUsUser";
+import ConstactUsCompany from "@/emails/ContactUsCompany";
+import { NextResponse } from "next/server";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(request: Request) {
+  const { name, tel, email, location, message, path, policy  } = await request.json();
+
+  try {
+
+  const userMail = await resend.sendEmail({
+    from: 'Maja from Acceptrec <maja@acceptrec.co.uk>',
+    to: email,
+    subject: `Hello ${name}`,
+    react: ContactUsUser({
+      name,
+      tel,
+      email,
+      location,
+      message
+    })
+  });
+
+  const companyMail = await resend.sendEmail({
+    from: 'Acceptrec.co.uk <hello@acceptrec.co.uk>',
+    to: 'maja@acceptrec.co.uk',
+    subject: 'New contact from the website',
+    react: ConstactUsCompany({
+      name,
+      tel,
+      email,
+      location,
+      message,
+      path,
+      policy
+    })
+  });
+  return NextResponse.json({
+    status: 'Ok'
+  }, {
+    status: 200
+  })
+  } catch(e: unknown) {
+    if (e instanceof Error) {
+      console.log(`Failed to send email: ${e.message}`);
+    }
+    return NextResponse.json({
+      error: 'Internal server error.'
+    }, {
+      status: 500
+    })
+  }
+}
