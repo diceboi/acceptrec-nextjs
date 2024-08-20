baf8eff74d839083d29417a54ad0090fff17a561
diceboi <szasz.szabolcs1995@gmail.com>
2024-08-20 10:58:32 +0200

    googlemaps api error try2

--- app/components/ChatMessage.tsx
+++ app/components/ChatMessage.tsx
@@ -11,7 +11,7 @@ export default function ChatMessage() {
     transition={{ duration: 10, times: [0, 0.05, 0.95, 1], ease: "circInOut" }}
     className='fixed flex flex-nowrap items-center gap-4 bottom-[48px] right-[100px] h-[52px] bg-[#00afaa] z-50 rounded-full p-4'>
         <div className='flex flex-col'>
-            <h4 className='font-bold text-white text-md'>Chat with us </h4>
+            <h4 className='font-bold text-white text-md'>Chat with us here</h4>
             <p className='text-sm text-white lg:block hidden'>for quick answers to most questions</p>
         </div>
         <FiArrowRight className='w-6 h-6 text-white'/>
--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
             <Cookies />
             <MainNav/>
             {children}
-            <ChatMessage />
+
             <MainCTA/>
             <Footer/>
           </ApolloWrapper>
